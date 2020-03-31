0f2d80465084e9efb70903d711e7dd7e2a7432de
Tam Ton <tam.ton@apps-cyclone.com>
2020-03-31 18:41:56 +0700

    init sourcecode

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"./controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.Static("/public", "./public")
+
+	client := r.Group("/api")
+	{
+		client.GET("/story/:id", controllers.Read)
+		client.POST("/story/create", controllers.Create)
+		client.PATCH("/story/update/:id", controllers.Update)
+		client.DELETE("/story/:id", controllers.Delete)
+	}
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	r.Run(":2301") // Ứng dụng chạy tại cổng 2301
+}
